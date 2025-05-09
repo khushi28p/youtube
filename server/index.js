6da7ae6e5de91ec5e6bdcdf8d3bfe8ebf7420810
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-05-09 08:34:13 +0530

    comment routes added

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 import { ConnectDB } from './config/db.config.js';
 import userRoutes from './routes/user.routes.js';
 import videoRoutes from './routes/video.routes.js';
+import commentRoutes from './routes/comment.routes.js'
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(fileUpload({
 
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/video', videoRoutes);
+app.use('/api/v1/comment', commentRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
